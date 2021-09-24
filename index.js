fde611ff45370a56db6c99f44b2069154e22862f
padosum <makalu171@gmail.com>
2021-09-24 11:32:52 +0900

    Protect secret information

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const { User } = require('./models/User');
+const config = require('./config/key');
 
-const mongoose = require('mongoose') 
-mongoose.connect('mongodb+srv://padosum:abcd1234@nodereact.67dvx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+const mongoose = require('mongoose');
+mongoose.connect(config.mongoURI, {
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
 
-
 app.get('/', (req, res) => {
   res.send('👋 Hello World!')
+});
+
+app.post('/register', (req, res) => {
+  const user = new User(req.body);
+  user.save((err, useInfo) => {
+    if(err) return res.json({ success: false, err});
+    return res.status(200).json({
+      success: true
+    })
+  })
 });
 
 app.listen(port, () => {
